69c8b115eb1d59a5a2020a071aa2a61239db9e6b
ravivarmans <s.ravivarman@elsevier.com>
2018-11-28 23:20:35 +0000

    Add the batch

---
+++ src/main/java/com/eph/automation/testing/models/contexts/LoadBatchContext.java
@@ -0,0 +1,19 @@
+package com.eph.automation.testing.models.contexts;
+
+import com.eph.automation.testing.annotations.StaticInjection;
+import com.eph.automation.testing.models.Product;
+
+/**
+ * Created by RAVIVARMANS on 28/11/2018.
+ */
+@StaticInjection
+public class LoadBatchContext {
+
+    public static String batchId;
+    public static String oldBatchId;
+    public static String loadId;
+    public static String oldLooadId;
+    public static boolean jobStatus;
+
+    public static Product productDetailsToCreate;
+}
