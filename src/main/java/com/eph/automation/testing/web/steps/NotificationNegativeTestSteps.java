e8c585f9eb7a395ec4313c53b1bb15052eca0170
Chuchukovn <n.chuchukov@elsevier.com>
2019-04-15 15:15:05 +0300

    added new negative tests for notifications

--- src/main/java/com/eph/automation/testing/web/steps/NotificationNegativeTestSteps.java
+++ src/main/java/com/eph/automation/testing/web/steps/NotificationNegativeTestSteps.java
@@ -91,7 +91,19 @@ public class NotificationNegativeTestSteps {
 
     @Then("^The product notification is not processed$")
     public void checkNotificationStatus() throws InterruptedException {
-        Thread.sleep(30 * 1000);
+        int j = 0;
+        sql= NotificationsSQL.EPH_GET_Notify_Status.replace("PARAM1",loadBatchContext.batchId);
+        do {
+            Thread.sleep(1000);
+            j++;
+            status= DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
+        }while(status.get(0).status.equalsIgnoreCase("UNPROCESSED") && j<30);
+        if (status.get(0).status.equalsIgnoreCase("UNPROCESSED")){
+            Assert.fail("The notification was not processed!");
+        }else{
+            Log.info("The notification was processed");
+        }
+
         sql = NotificationsSQL.EPH_GET_Notification_Neg.replace("PARAM1", loadBatchContext.batchId);
         notificationCountContext.getStatusSTNotification = DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
 
@@ -117,7 +129,7 @@ public class NotificationNegativeTestSteps {
         } else {
             attemptsAfter = notificationCountContext.getWriteAttemptsAfterNegProduct1.get(0).attempts;
         }
-        Log.info("The product 1 attempts before update are: " + attemptsAfter);
+        Log.info("The product 1 attempts after update are: " + attemptsAfter);
 
         Assert.assertEquals("The product was updated", notificationCountContext.getWriteAttemptsBeforeNegProduct1.get(0).attempts,
                 notificationCountContext.getWriteAttemptsBeforeNegProduct1.get(0).attempts);
@@ -130,7 +142,7 @@ public class NotificationNegativeTestSteps {
         } else {
             attemptsAfter = notificationCountContext.getWriteAttemptsAfterNegProduct2.get(0).attempts;
         }
-        Log.info("The product 1 attempts before update are: " + attemptsAfter);
+        Log.info("The product 1 attempts after update are: " + attemptsAfter);
 
         Assert.assertEquals("The product was updated", notificationCountContext.getWriteAttemptsBeforeNegProduct2.get(0).attempts,
                 notificationCountContext.getWriteAttemptsBeforeNegProduct2.get(0).attempts);
@@ -177,7 +189,19 @@ public class NotificationNegativeTestSteps {
 
     @Then("^The work and product notifications are not processed$")
     public void checkNotifications() throws InterruptedException {
-        Thread.sleep(30 * 1000);
+        int j = 0;
+        sql= NotificationsSQL.EPH_GET_Notify_Status.replace("PARAM1",loadBatchContext.batchId);
+        do {
+            Thread.sleep(1000);
+            j++;
+            status= DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
+        }while(status.get(0).status.equalsIgnoreCase("UNPROCESSED") && j<30);
+        if (status.get(0).status.equalsIgnoreCase("UNPROCESSED")){
+            Assert.fail("The notification was not processed!");
+        }else{
+            Log.info("The notification was processed");
+        }
+
         sql = NotificationsSQL.EPH_GET_Notification_Neg.replace("PARAM1", loadBatchContext.batchId);
         notificationCountContext.getStatusSTNotification = DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
 
@@ -204,4 +228,132 @@ public class NotificationNegativeTestSteps {
         }
     }
 
+    @Given("^A correct manifestation is updated connected to incorrect product$")
+    public void manifestationUpdate() {
+        sql = NotificationsSQL.EPH_GET_Write_Attempts.replace("PARAM1", "EPR-W-TSTW10:RBK");
+        notificationCountContext.getWriteAttemptsBeforeNegWork =
+                DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
+        if (notificationCountContext.getWriteAttemptsBeforeNegWork.isEmpty()) {
+            attemptsBefore = 0;
+        } else {
+            attemptsBefore = notificationCountContext.getWriteAttemptsBeforeNegWork.get(0).attempts;
+        }
+        Log.info("The work attempts before update are: " + attemptsBefore);
+
+        sql = NotificationsSQL.EPH_GET_Write_Attempts.replace("PARAM1", "EPR-TSTP10:OOA");
+        notificationCountContext.getWriteAttemptsBeforeNegProduct1 =
+                DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
+        if (notificationCountContext.getWriteAttemptsBeforeNegProduct1.isEmpty()) {
+            attemptsBefore = 0;
+        } else {
+            attemptsBefore = notificationCountContext.getWriteAttemptsBeforeNegProduct1.get(0).attempts;
+        }
+        Log.info("The product 1 attempts before update are: " + attemptsBefore);
+
+        sql = NotificationsSQL.EPH_GET_Write_Attempts.replace("PARAM1", "EPR-TSTP20:OOA");
+        notificationCountContext.getWriteAttemptsBeforeNegProduct2 =
+                DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
+        if (notificationCountContext.getWriteAttemptsBeforeNegProduct2.isEmpty()) {
+            attemptsBefore = 0;
+        } else {
+            attemptsBefore = notificationCountContext.getWriteAttemptsBeforeNegProduct2.get(0).attempts;
+        }
+        Log.info("The product 2 attempts before update are: " + attemptsBefore);
+
+        loadBatchContext.batchId = DataLoadServiceImpl.updateManifestation1();
+        System.out.println(loadBatchContext.batchId);
+        JobUtils.waitTillTheBatchComplete();
+    }
+
+    @Given("^A correct manifestation is updated connected to incorrect work and product$")
+    public void manifestationIsUpdated() {
+        sql = NotificationsSQL.GET_FAILED_NOT_PAYLOAD;
+        notificationCountContext.getFailedPayloadNotBefore =
+                DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
+        if (notificationCountContext.getFailedPayloadNotBefore.isEmpty()){
+            Log.info("There are no processed notifications");
+        }else{
+            Log.info("The created notifications before are: " + notificationCountContext.getFailedPayloadNotBefore.size());
+        }
+
+        loadBatchContext.batchId = DataLoadServiceImpl.updateManifestation2();
+        System.out.println(loadBatchContext.batchId);
+        JobUtils.waitTillTheBatchComplete();
+    }
+
+    @Given("^An incorrect product is updated connected to correct work$")
+    public void productUpdate() {
+        sql = NotificationsSQL.EPH_GET_Write_Attempts.replace("PARAM1", "EPR-W-TSTW10:RBK");
+        notificationCountContext.getWriteAttemptsBeforeNegWork =
+                DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
+        if (notificationCountContext.getWriteAttemptsBeforeNegWork.isEmpty()) {
+            attemptsBefore = 0;
+        } else {
+            attemptsBefore = notificationCountContext.getWriteAttemptsBeforeNegWork.get(0).attempts;
+        }
+        Log.info("The work attempts before update are: " + attemptsBefore);
+
+        sql = NotificationsSQL.EPH_GET_Write_Attempts.replace("PARAM1", "EPR-TSTP10:OOA");
+        notificationCountContext.getWriteAttemptsBeforeNegProduct1 =
+                DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
+        if (notificationCountContext.getWriteAttemptsBeforeNegProduct1.isEmpty()) {
+            attemptsBefore = 0;
+        } else {
+            attemptsBefore = notificationCountContext.getWriteAttemptsBeforeNegProduct1.get(0).attempts;
+        }
+        Log.info("The product 1 attempts before update are: " + attemptsBefore);
+
+        sql = NotificationsSQL.EPH_GET_Write_Attempts.replace("PARAM1", "EPR-TSTP20:OOA");
+        notificationCountContext.getWriteAttemptsBeforeNegProduct2 =
+                DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
+        if (notificationCountContext.getWriteAttemptsBeforeNegProduct2.isEmpty()) {
+            attemptsBefore = 0;
+        } else {
+            attemptsBefore = notificationCountContext.getWriteAttemptsBeforeNegProduct2.get(0).attempts;
+        }
+        Log.info("The product 2 attempts before update are: " + attemptsBefore);
+
+        loadBatchContext.batchId = DataLoadServiceImpl.updateProduct1();
+        System.out.println(loadBatchContext.batchId);
+        JobUtils.waitTillTheBatchComplete();
+    }
+
+    @Then("^The failed product notification is not processed$")
+    public void checkProductNotificationStatus() throws InterruptedException {
+        int j = 0;
+        sql= NotificationsSQL.EPH_GET_Notify_Status.replace("PARAM1",loadBatchContext.batchId);
+        do {
+            Thread.sleep(1000);
+            j++;
+            status= DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
+        }while(status.get(0).status.equalsIgnoreCase("UNPROCESSED") && j<30);
+        if (status.get(0).status.equalsIgnoreCase("UNPROCESSED")){
+            Assert.fail("The notification was not processed!");
+        }else{
+            Log.info("The notification was processed");
+        }
+
+        sql = NotificationsSQL.EPH_GET_Notification_Neg.replace("PARAM1", loadBatchContext.batchId);
+        notificationCountContext.getStatusSTNotification = DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
+
+        for (int i = 0; i < notificationCountContext.getStatusSTNotification.size(); i++) {
+            Assert.assertEquals("The notifications are not in Failed status",
+                    "ERROR",
+                    notificationCountContext.getStatusSTNotification.get(i).status);
+        }
+
+        sql = NotificationsSQL.EPH_GET_Write_Attempts.replace("PARAM1", "EPR-TSTP10:OOA");
+        notificationCountContext.getWriteAttemptsAfterNegProduct1 =
+                DBManager.getDBResultAsBeanList(sql, NotificationDataObject.class, Constants.EPH_URL);
+        if (notificationCountContext.getWriteAttemptsAfterNegProduct1.isEmpty()) {
+            attemptsAfter = 0;
+        } else {
+            attemptsAfter = notificationCountContext.getWriteAttemptsAfterNegProduct1.get(0).attempts;
+        }
+        Log.info("The product 1 attempts after update are: " + attemptsAfter);
+
+        Assert.assertEquals("The product was updated", notificationCountContext.getWriteAttemptsBeforeNegProduct1.get(0).attempts,
+                notificationCountContext.getWriteAttemptsBeforeNegProduct1.get(0).attempts);
+
+    }
 }
