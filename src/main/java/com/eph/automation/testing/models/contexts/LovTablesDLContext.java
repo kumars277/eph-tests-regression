bf5a9b5ab27dd7902c5c2c555e61b4179a67157f
Kruck <KRUCKT@science.regn.net>
2020-02-21 09:26:09 +0000

    Lov tables data checks

---
+++ src/main/java/com/eph/automation/testing/models/contexts/LovTablesDLContext.java
@@ -0,0 +1,9 @@
+package com.eph.automation.testing.models.contexts;
+
+import com.eph.automation.testing.models.dao.datalake.*;
+import java.util.List;
+
+public class LovTablesDLContext {
+    public static List<LovTableDLObject> LovTableDataObjectsFromEPH;
+    public static List<LovTableDLObject> LovTableDataObjectsFromDL;
+}
