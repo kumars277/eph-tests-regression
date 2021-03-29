d8f69445d957f586a4093b5b52c4b9fbac0b70ed
Nishant Chitre <n.chitre@elsevier.com>
2021-03-29 13:07:07 +0530

    JM-UI Firefox driver installed

--- src/main/java/com/eph/automation/testing/models/TestContext.java
+++ src/main/java/com/eph/automation/testing/models/TestContext.java
@@ -4,6 +4,7 @@ import com.eph.automation.testing.annotations.StaticInjection;
 
 /**
  * Created by RAVIVARMANS on 11/24/2018.
+ * updated by Nishant @ 18 Mar 2021 for JM UI automation
  */
 @StaticInjection
 public class TestContext {
@@ -11,8 +12,8 @@ public class TestContext {
     public static TestProperties getValues() {
         TestProperties testProperties = new TestProperties();
         System.setProperty("ENV","SIT");
+        System.setProperty("browser","firefox");
 
-       // System.setProperty("browser","chrome");
         String browserName = System.getProperty("browser");
         String gridRun = System.getProperty("gridRun");
         String targetDB = System.getProperty("targetDB");
