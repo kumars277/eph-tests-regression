d77ff332a0e048f2ae5b633e593a28e409a8834f
Dinesh <67102235+sureshkumardinesh@users.noreply.github.com>
2021-04-13 16:15:45 +0530

    Update RunBDDTest.java

--- src/test/java/runner/RunBDDTest.java
+++ src/test/java/runner/RunBDDTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(strict = false, features = "src/main/resources/features", format = { "pretty",
-        "json:target/cucumber.json" }, tags = { "@test123" },
+        "json:target/cucumber.json" }, tags = { "@stitchingData" },
         glue = {"com.eph.automation.testing.web.steps", "com.eph.automation.testing.common.hooks"})
 public class RunBDDTest {
 }
