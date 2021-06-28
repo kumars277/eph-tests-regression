afd0e9813720bc539d27265bd45b2a34ac4e0316
@SURESHKUMARD <d.sureshkumard@elsevier.com>
2021-06-28 12:17:40 +0530

    gd to stitching core

--- src/main/java/com/eph/automation/testing/steps/jm/JMETLDataChecks.java
+++ src/main/java/com/eph/automation/testing/steps/jm/JMETLDataChecks.java
@@ -1757,7 +1757,7 @@ public class JMETLDataChecks {
     //Comparing Core records between Semarchy and Stiching
     @Given("^We get the (.*) random Stitching ids of (.*)$")
     public void getRandomStitchingIds(String numberOfRecords, String SemarchyTable) {
-//  numberOfRecords = System.getProperty("dbRandomRecordsNumber"); //Uncomment when running in jenkins
+  numberOfRecords = System.getProperty("dbRandomRecordsNumber"); //Uncomment when running in jenkins
         Log.info("numberOfRecords = " + numberOfRecords);
         Log.info("Getting random records...");
         switch (SemarchyTable) {
