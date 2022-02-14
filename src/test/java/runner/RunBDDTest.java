bca4a75bb15a00c21489846d7298e7b40a2ff985
Nishant Chitre <n.chitre@elsevier.com>
2022-02-14 11:27:06 +0530

    cucumber report fix

--- src/test/java/runner/RunBDDTest.java
+++ src/test/java/runner/RunBDDTest.java
@@ -12,9 +12,9 @@ import org.junit.runner.RunWith;
 
         glue = {"com.eph.automation.testing.steps", "com.eph.automation.testing.common.hooks"},
         features = "src/main/resources/features",
-        plugin = {"pretty", "json:target/cucumber-reports/cucumber.json",
-                "junit:target/cucumber-reports/Cucumber.xml",
-                "html:target/cucumber-reports/feature-overview"
+        plugin = {"pretty", "json:target/reports/cucumber.json",
+                           "junit:target/reports/Cucumber.xml",
+                            "html:target/reports/feature-overview"
                 },
         monochrome = true,
         tags = {"@stitchingDataAPI"}
