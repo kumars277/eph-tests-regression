77c0630c52db8b15a3a9d2bd652f529adc95522d
@SURESHKUMARD <d.sureshkumard@elsevier.com>
2022-05-05 19:48:39 +0530

    r12_view_dag

--- src/main/java/com/eph/automation/testing/models/contexts/GDTablesDLSQLContext.java
+++ src/main/java/com/eph/automation/testing/models/contexts/GDTablesDLSQLContext.java
@@ -2,6 +2,7 @@ package com.eph.automation.testing.models.contexts;
 
 import com.eph.automation.testing.annotations.StaticInjection;
 import com.eph.automation.testing.models.dao.GDTableDLSQL.GDTableDLSQLObject;
+import com.eph.automation.testing.models.dao.consumerApp.r12Objects;
 
 import java.util.List;
 
@@ -13,5 +14,7 @@ public class GDTablesDLSQLContext {
     public static List<GDTableDLSQLObject>recordsFromDL;
     public static List<GDTableDLSQLObject> recordsFromGDTableIdentifiers;
     public static List<GDTableDLSQLObject> recordsFromCrossRefIdentifier;
+    public static List<r12Objects>recsR12source;
+    public static List<r12Objects>recsR12target;
 }
 
