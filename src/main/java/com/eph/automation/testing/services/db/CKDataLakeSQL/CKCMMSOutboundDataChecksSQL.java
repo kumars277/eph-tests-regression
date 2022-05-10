5c7b2f866730494531e4f104805ff3f325f0edcb
kruckt <65550547+kruckt@users.noreply.github.com>
2022-05-10 11:11:29 +0100

    repairing CMMS Test

--- src/main/java/com/eph/automation/testing/services/db/CKDataLakeSQL/CKCMMSOutboundDataChecksSQL.java
+++ src/main/java/com/eph/automation/testing/services/db/CKDataLakeSQL/CKCMMSOutboundDataChecksSQL.java
@@ -12,7 +12,7 @@ public class CKCMMSOutboundDataChecksSQL {
     //     GET View Records
     public static String GET_CK_CMMS_WORKFLOW_VIEW = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where workflowid in (%s)";
     public static String GET_CK_CMMS_WORK1_VIEW = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where currentIdentifier in ('%s')";
-    public static String GET_CK_CMMS_WORK2_IDENTIFIERS_VIEW = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where identifier in ('%s')";
+    public static String GET_CK_CMMS_WORK2_IDENTIFIERS_VIEW = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where identifier in ('%s') order by workId desc, identifier desc ";
     public static String GET_CK_CMMS_WORK3_SUBJECT_AREAS_VIEW = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where subjectAreaId in (%s) order by enddate desc, workid desc ";
     public static String GET_CK_CMMS_PACKAGE1_VIEW = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where packageId in ('%s')";
     public static String GET_CK_CMMS_PACKAGE2_WORKS_VIEW = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where workId in ('%s') order by durableurl desc";
@@ -20,7 +20,7 @@ public class CKCMMSOutboundDataChecksSQL {
     //    GET Table Records
     public static String GET_CK_CMMS_WORKFLOW_TABLE = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where workflowid in (%s)";
     public static String GET_CK_CMMS_WORK1 = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where currentIdentifier in ('%s')";
-    public static String GET_CK_CMMS_WORK2_IDENTIFIERS = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where identifier in ('%s')";
+    public static String GET_CK_CMMS_WORK2_IDENTIFIERS = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where identifier in ('%s') order by workId desc, identifier desc";
     public static String GET_CK_CMMS_WORK3_SUBJECT_AREAS = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where subjectAreaId in (%s) order by enddate desc, workid desc";
     public static String GET_CK_CMMS_PACKAGE1 = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where packageId in ('%s')";
     public static String GET_CK_CMMS_PACKAGE2_WORKS = "select * from " + GetCKDLDB.getCKCMMSDataBase() + ".%s where workId in ('%s') order by durableurl desc";
