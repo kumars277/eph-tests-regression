96b6041bbcb616334fe51c7ebf836c70d85bb497
@SURESHKUMARD <d.sureshkumard@elsevier.com>
2023-01-03 15:12:09 +0530

    dpp_cmms_export dag fix

--- src/main/java/com/eph/automation/testing/steps/ck/CKCMMSExportCountChecksSteps.java
+++ src/main/java/com/eph/automation/testing/steps/ck/CKCMMSExportCountChecksSteps.java
@@ -13,36 +13,74 @@ import java.util.List;
 import java.util.Map;
 
 public class CKCMMSExportCountChecksSteps {
-    private static String CKSQL;
+    private static String CKInboundSQL;
+    private static String CKSQLView;
     private static int CK_CMMS_ViewCount;
     private static int CK_CMMS_TableCount;
 
     //CMMS View to Table count checks
-    @Given("We know the number of CK (.*) data in CMMS Outbound View")
+    @Given("Get the count from the inbound for (.*)")
     public void getCKCMMSViewCount(String DPPCMMSView) {
-        CKSQL = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_VIEW_COUNT, DPPCMMSView);
-        Log.info(CKSQL);
-        List<Map<String, Object>> CK_CMMSViewCount = DBManager.getDLResultMap(CKSQL, Constants.AWS_URL);
-        CK_CMMS_ViewCount = ((Long) CK_CMMSViewCount.get(0).get("count")).intValue();
-        Log.info(DPPCMMSView + " table in CK CMMS View has the Count: " + CK_CMMS_ViewCount);
+
+        switch (DPPCMMSView){
+            case "cmms_workflow_v":
+                CKInboundSQL = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_WORKFLOW_VIEW_INBOUND_COUNT);
+                break;
+            case "cmms_work1_v":
+                CKInboundSQL = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_WORK1_INBOUND_COUNT);
+                break;
+            case "cmms_work2_identifiers_v":
+                CKInboundSQL = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_WORK2_IDENTIFIERS_INBOUND_COUNT);
+                break;
+            case "cmms_work3_subject_areas_v":
+                CKInboundSQL = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_WORK3_SUBJ_AREA_INBOUND_COUNT);
+                break;
+            case "cmms_package1_v":
+                CKInboundSQL = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_PKG1_INBOUND_COUNT);
+                break;
+            case "cmms_package2_works_v":
+                CKInboundSQL = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_PKG2_WORKS_INBOUND_COUNT);
+                break;
+        }
+        Log.info(CKInboundSQL);
+        List<Map<String, Object>> CK_CMMSViewCount = DBManager.getDLResultMap(CKInboundSQL, Constants.AWS_URL);
+        CK_CMMS_ViewCount = ((Long) CK_CMMSViewCount.get(0).get("COUNT")).intValue();
     }
 
-    @Then("^Get the count for CK (.*) CMMS Table")
-    public void getCKCMMSTableCount(String DPPCMMSTable) {
-        CKSQL = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_Table_COUNT, DPPCMMSTable);
-        Log.info(CKSQL);
-        List<Map<String, Object>> CKCMMSTableCount = DBManager.getDLResultMap(CKSQL, Constants.AWS_URL);
-        CK_CMMS_TableCount = ((Long) CKCMMSTableCount.get(0).get("Count")).intValue();
-        Log.info(DPPCMMSTable + " table in CK Delta Current has the Count: " + CK_CMMS_TableCount);
+    @Then("^Get the count for (.*) views")
+    public void getCKCMMSTableCount(String DPPCMMSView) {
+        switch (DPPCMMSView){
+            case "cmms_workflow_v":
+                CKSQLView = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_WORKFLOW_VIEW_COUNT);
+                break;
+            case "cmms_work1_v":
+                CKSQLView = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_WORK1_VIEW_COUNT);
+                break;
+            case "cmms_work2_identifiers_v":
+                CKSQLView = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_WORK2_IDENTIFIERS_VIEW_COUNT);
+                break;
+            case "cmms_work3_subject_areas_v":
+                CKSQLView = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_WORK3_SUBJ_AREA_VIEW_COUNT);
+                break;
+            case "cmms_package1_v":
+                CKSQLView = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_PKG1_VIEW_COUNT);
+                break;
+            case "cmms_package2_works_v":
+                CKSQLView = String.format(CKCMMSExportCountCheckSQL.GET_CMMS_PKG2_WORKS_VIEW_COUNT);
+                break;
+        }
+        Log.info(CKSQLView);
+        List<Map<String, Object>> CKCMMSTableCount = DBManager.getDLResultMap(CKSQLView, Constants.AWS_URL);
+        CK_CMMS_TableCount = ((Long) CKCMMSTableCount.get(0).get("COUNT")).intValue();
     }
 
-    @And("^Compare the CK count for (.*) table between CMMS View and CMMS Table$")
+    @And("^Compare the count between the inbounds and (.*) views$")
     public void CKcompareCMMSViewtoTable(String DPPCMMSTable) {
         if (CK_CMMS_TableCount == 0) {
             Log.info("Delta Current table empty Count is " + CK_CMMS_TableCount);
         } else {
-            Log.info("The count for table" + DPPCMMSTable + " in CK CMMS View: " + CK_CMMS_ViewCount + " and CK CMMS Table: " + CK_CMMS_TableCount);
-            Assert.assertEquals("The counts for table " + DPPCMMSTable + " is not equal", CK_CMMS_ViewCount, CK_CMMS_TableCount);
+            Log.info("The Inbound count for " + DPPCMMSTable + ": "+CK_CMMS_ViewCount+" and in the views of "+DPPCMMSTable+": "+CK_CMMS_TableCount);
+            Assert.assertEquals("The Inbound counts and " + DPPCMMSTable + " is not equal", CK_CMMS_ViewCount, CK_CMMS_TableCount);
         }
     }
 }
